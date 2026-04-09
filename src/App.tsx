f904f94e58f2625468e205e303ff88d7d90eceb0
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-04-09 03:57:25 -0400

    correcion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import Messages from "./components/Messages";
+import "./App.css";
+import Conversations from "./components/Conversations";
+import ConversationsList from "./components/ConversationList";
+import ChatWindow from "./components/ChatWindow";
+import Sidebar from "./components/Sidebar";
+import MessageInput from "./components/MessageInput";
 
 function App() {
-  return <Messages />;
+  const [selectedConversationId, setSelectedConversationId] = useState<number | null>(null);
+
+  const handleMessagesUpdate = () => {};
+
+  return (
+    <div className="App">
+      {selectedConversationId !== null && (
+        <MessageInput
+          conversationId={selectedConversationId}
+          onMessagesUpdate={handleMessagesUpdate}
+        />
+      )}
+      <Conversations />
+      <Sidebar onSelect={setSelectedConversationId} /> 
+      <Messages />
+      <ConversationsList onSelect={setSelectedConversationId} />
+      {selectedConversationId && <ChatWindow conversationId={selectedConversationId} />}
+
+    </div>
+  );
 }
 
 export default App;

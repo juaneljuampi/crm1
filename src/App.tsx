1234d732dde348c5b39129b522dddf6c8d37e81b
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-04-09 07:24:30 -0400

    correcion

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useState } from "react";
-import Messages from "./components/Messages";
 import "./App.css";
-import Conversations from "./components/Conversations";
-import ConversationsList from "./components/ConversationList";
-import ChatWindow from "./components/ChatWindow";
-import Sidebar from "./components/Sidebar";
-import MessageInput from "./components/MessageInput";
-
+import Formulario from "./pages/Formulario";
 function App() {
-  const [selectedConversationId, setSelectedConversationId] = useState<number | null>(null);
-
-  const handleMessagesUpdate = () => {};
 
   return (
     <div className="App">
-      {selectedConversationId !== null && (
-        <MessageInput
-          conversationId={selectedConversationId}
-          onMessagesUpdate={handleMessagesUpdate}
-        />
-      )}
-      <Conversations />
-      <Sidebar onSelect={setSelectedConversationId} /> 
-      <Messages />
-      <ConversationsList onSelect={setSelectedConversationId} />
-      {selectedConversationId && <ChatWindow conversationId={selectedConversationId} />}
-
+      <Formulario />
     </div>
   );
 }

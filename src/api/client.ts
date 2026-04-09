f3ded29f09240a284433bec63adb5b8e82ae7b6c
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-04-09 03:07:38 -0400

    correcion

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://129.212.191.110:3000", // backend en DigitalOcean
+  baseURL: "http://129.212.191.110:3000/api/", // backend en DigitalOcean
   headers: { "Content-Type": "application/json" }
 });
 
